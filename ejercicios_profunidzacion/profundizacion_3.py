baf2383474267180f7cbceef04ef6d9a3f970bce
LucasEckart <115913096+LucasEckart@users.noreply.github.com>
2022-11-16 16:30:37 -0300

    Add files via upload

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -4,7 +4,7 @@
 # Autor: Inove Coding School
 # Version: 2.2
 
-# NOTA: 
+# NOTA:
 # Estos ejercicios son de mayor dificultad que los de clase y práctica.
 # Están pensados para aquellos con conocimientos previo o que dispongan
 # de mucho más tiempo para abordar estos temas por su cuenta.
@@ -37,9 +37,75 @@ avanzado adicional que se encuentra en otro repo de inove:
 Link al repo:
 https://github.com/InoveAlumnos/generala_python
 
-IMPORTANTE: Encarar ese desafio es algo personal de cada uno, 
+IMPORTANTE: Encarar ese desafio es algo personal de cada uno,
 de nuestro lado preferimos que pongan su foco en tener los conceptos asimilados
 para la próxima clase, para poder comenzar a debatir sobre los proyectos
 
 '''
+import random
+
+def dados(cantidad):
+    dados = []
+    for tirada in range(cantidad):
+        dado = random.randint(1, 6)
+        dados.append(dado)
+    return dados
+
+
+def numero_repetido (dados_tirados):
+    mas_repetido = max(dados_tirados, key = dados_tirados.count)
+    return mas_repetido
+
+
+def cantidad_repetidos(lista, objetivo, dados_guardados):
+    for dado in lista:
+        if dado == objetivo:
+            dados_guardados.append(dado)
+    return dados_guardados            
+    
+
+
+if __name__ == "__main__":
+    
+    dados_guardados = []
+
+    tirar_dados = dados (5)
+    print(f'Primera tirada: {tirar_dados}')
+
+    dado_repetido = numero_repetido(tirar_dados)
+    print(f'Número repetido: {dado_repetido}')
+
+    dados_guardados = cantidad_repetidos (tirar_dados, dado_repetido, dados_guardados)
+    print(f'Dados guardados en la primera tirada: {dados_guardados}\n-------')
+
+    if len(dados_guardados) == 5:
+        print(F'GENERALA! FELICIDADES GANASTE EL JUEGUIO VIEJA SOS UN GRANDE!')
+        exit()
+
+
+    tirada_2 =  5 - len(dados_guardados)
+    tirar_dados = dados(tirada_2)
+    print(f'Segunda tirada: {tirar_dados}')
+    
+    dados_guardados = cantidad_repetidos (tirar_dados, dado_repetido, dados_guardados)
+    print(f'Dados guardados en la segunda tirada: {dados_guardados}\n-------')
+
+    if len(dados_guardados) == 5:
+        print(F'GENERALA! FELICIDADES GANASTE EL JUEGUIO VIEJA SOS UN GRANDE!')
+        exit()
+
+   
+    tirada_3 =  5 - len(dados_guardados)
+    tirar_dados = dados(tirada_3)
+    print(f'Tercera tirada: {tirar_dados}')
+    
+    dados_guardados = cantidad_repetidos (tirar_dados, dado_repetido, dados_guardados)
+    print(f'Dados guardados en la tercera tirada: {dados_guardados}')
+
+    if len(dados_guardados) == 5:
+        print(F'GENERALA! FELICIDADES GANASTE EL JUEGUIO VIEJA SOS UN GRANDE!')
+        exit()
+    else:
+        print(f'Perdiste')
+
 
